e6be7ec77bad66bbb137a218e97a6b31186a514a
RilanRMZ <143393996+RilanRMZ@users.noreply.github.com>
2025-05-19 19:59:45 +0400

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -17,6 +17,10 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/v1', api);
 
+app.get('/', (req, res) => {
+  res.send("Welcome to NASA Planet project")
+})
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
