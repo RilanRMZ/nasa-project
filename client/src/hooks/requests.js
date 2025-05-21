3ab6d35e8e742c67e2432ab09504ef254540182d
RilanRMZ <143393996+RilanRMZ@users.noreply.github.com>
2025-05-21 09:46:53 +0400

    Update requests.js

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-const API_URL = "https://nasa-project-server-two.vercel.app/v1";
+const API_URL = "https://nasa-project-server-chi.vercel.app/v1";
 
 // Load planets and return as JSON.
 async function httpGetPlanets() {
