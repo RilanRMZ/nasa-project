559132a8da0b2493ea044ab87fe86e33395fc579
RilanRMZ <143393996+RilanRMZ@users.noreply.github.com>
2025-05-21 09:50:20 +0400

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const api = require('./routes/api');
 const app = express();
 
 app.use(cors({
-  origin: 'https://nasa-project-client-beta.vercel.app', credentials:true,
+  origin: 'https://nasa-project-client-beta.vercel.app',
 }));
 app.use(morgan('combined'));
 
