8e5e2d6341593f251ad3223d19cd8790d1cbe3d0
RilanRMZ <rilan.rmz@gmail.com>
2025-05-22 14:33:52 +0400

    modified mongodb uri

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 
 // Update below to match your own MongoDB connection string.
 const MONGO_URL =
-  "mongodb+srv://nasa-api:GlOghOJIw83PB27U@nasacluster.imqfmsj.mongodb.net/nasa?retryWrites=true&w=majority";
+  "mongodb://nasa-api:GlOghOJIw83PB27U@ac-ry9akvx-shard-00-00.imqfmsj.mongodb.net:27017,ac-ry9akvx-shard-00-01.imqfmsj.mongodb.net:27017,ac-ry9akvx-shard-00-02.imqfmsj.mongodb.net:27017/nasa?ssl=true&replicaSet=atlas-13c8lx-shard-0&authSource=admin&retryWrites=true&w=majority&appName=NASAcluster";
+// "mongodb+srv://nasa-api:GlOghOJIw83PB27U@nasacluster.imqfmsj.mongodb.net/nasa?retryWrites=true&w=majority";
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
